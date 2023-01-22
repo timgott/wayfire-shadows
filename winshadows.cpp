9658be595afd38bd66a7f6f29b6bd482ebde0a1e
Tim Göttlicher <timg00@outlook.de>
2023-01-22 19:23:52 +0100

    Fix issue caused by removed shadow node Sometimes all nodes are removed from the scene but the view's custom data stays. (accidentally formatted the file)

--- winshadows.cpp
+++ winshadows.cpp
@@ -1,13 +1,13 @@
+#include <wayfire/matcher.hpp>
 #include <wayfire/object.hpp>
+#include <wayfire/output.hpp>
+#include <wayfire/per-output-plugin.hpp>
 #include <wayfire/plugin.hpp>
+#include <wayfire/scene-operations.hpp>
+#include <wayfire/signal-definitions.hpp>
 #include <wayfire/signal-provider.hpp>
 #include <wayfire/view.hpp>
-#include <wayfire/matcher.hpp>
 #include <wayfire/workspace-manager.hpp>
-#include <wayfire/output.hpp>
-#include <wayfire/signal-definitions.hpp>
-#include <wayfire/scene-operations.hpp>
-#include <wayfire/per-output-plugin.hpp>
 
 #include "node.hpp"
 
@@ -22,55 +22,49 @@ namespace wayfire_shadows_globals {
     wayfire_view last_focused_view = nullptr;
 }
 
-class wayfire_shadows : public wf::per_output_plugin_instance_t
-{
+class wayfire_shadows : public wf::per_output_plugin_instance_t {
     const std::string surface_data_name = "shadow_surface";
 
-    wf::view_matcher_t enabled_views {"winshadows/enabled_views"};
-    wf::option_wrapper_t<bool> include_undecorated_views {"winshadows/include_undecorated_views"};
+    wf::view_matcher_t enabled_views{"winshadows/enabled_views"};
+    wf::option_wrapper_t<bool> include_undecorated_views{"winshadows/include_undecorated_views"};
 
-    wf::signal::connection_t<wf::view_mapped_signal> on_view_mapped = 
-        [=] (auto *data) { update_view_decoration(data->view);};
+    wf::signal::connection_t<wf::view_mapped_signal> on_view_mapped =
+        [=](auto *data) { update_view_decoration(data->view); };
     wf::signal::connection_t<wf::view_decoration_state_updated_signal> on_view_updated =
-        [=] (auto *data) { update_view_decoration(data->view);};
+        [=](auto *data) { update_view_decoration(data->view); };
     wf::signal::connection_t<wf::view_tiled_signal> on_view_tiled =
-        [=] (auto *data) { update_view_decoration(data->view);};
-    
+        [=](auto *data) { update_view_decoration(data->view); };
 
     wf::signal::connection_t<wf::focus_view_signal> on_focus_changed =
         [=](wf::focus_view_signal *data) {
             wayfire_view focused_view = data->view;
             wayfire_view last_focused = wayfire_shadows_globals::last_focused_view;
             if (last_focused != nullptr) {
-            update_view_decoration(last_focused);
+                update_view_decoration(last_focused);
             }
             if (focused_view != nullptr) {
-            update_view_decoration(focused_view);
+                update_view_decoration(focused_view);
             }
             wayfire_shadows_globals::last_focused_view = focused_view;
         };
 
     wf::signal::connection_t<wf::view_unmapped_signal> on_view_unmapped =
-        [=] (auto *data)
-        {
+        [=](auto *data) {
             wayfire_view view = data->view;
             if (view == wayfire_shadows_globals::last_focused_view) {
                 wayfire_shadows_globals::last_focused_view = nullptr;
             }
         };
 
-  public:
-    void init() override
-    {
+public:
+    void init() override {
         output->connect(&on_view_mapped);
         output->connect(&on_view_updated);
         output->connect(&on_view_tiled);
         output->connect(&on_focus_changed);
         output->connect(&on_view_unmapped);
 
-        for (auto& view :
-             output->workspace->get_views_in_layer(wf::ALL_LAYERS))
-        {
+        for (auto &view : output->workspace->get_views_in_layer(wf::ALL_LAYERS)) {
             update_view_decoration(view);
         }
     }
@@ -82,61 +76,59 @@ class wayfire_shadows : public wf::per_output_plugin_instance_t
      * @param view The view to match
      * @return Whether the view should get a shadow.
      */
-    bool is_view_shadow_enabled(wayfire_view view)
-    {
+    bool is_view_shadow_enabled(wayfire_view view) {
         return enabled_views.matches(view) && (is_view_decorated(view) || include_undecorated_views);
     }
 
-    bool is_view_decorated(wayfire_view view)
-    {
+    bool is_view_decorated(wayfire_view view) {
         return view->should_be_decorated();
     }
 
+    const wf::scene::floating_inner_ptr& get_shadow_root_node(wayfire_view view) const {
+        return view->get_surface_root_node();
+    }
+
     wf::wl_idle_call idle_deactivate;
-    void update_view_decoration(wayfire_view view)
-    {
-        if (is_view_shadow_enabled(view))
-        {
+    void update_view_decoration(wayfire_view view) {
+        if (is_view_shadow_enabled(view)) {
             auto shadow_data = view->get_data<view_shadow_data>(surface_data_name);
             if (!shadow_data) {
                 // No shadow yet, create it now.
                 init_view(view);
-            }
-            else {
+            } else {
+                // in some situations the shadow node might have been removed due to unmap, but the view is reused (including the custom data)
+                auto shadow_root = get_shadow_root_node(view);
+                if (shadow_data->shadow_ptr->parent() != shadow_root.get()) {
+                        wf::scene::add_back(shadow_root, shadow_data->shadow_ptr);
+                }
                 // Shadow already exists, redraw if necessary,
                 // e.g. view was focused and glow is enabled.
                 if (shadow_data->shadow_ptr->needs_redraw()) {
                     view->damage();
                 }
             }
-        } else
-        {
+        } else {
             deinit_view(view);
         }
     }
 
-    bool is_view_initialized( wayfire_view view ) {
+    bool is_view_initialized(wayfire_view view) {
         return view->has_data(surface_data_name);
     }
 
-    void init_view( wayfire_view view )
-    {
+    void init_view(wayfire_view view) {
         // create the shadow node and add it to the view
-        auto node = std::make_shared<winshadows::shadow_node_t>( view );
-        wf::scene::add_back(view->get_surface_root_node(), node);
+        auto node = std::make_shared<winshadows::shadow_node_t>(view);
+        wf::scene::add_back(get_shadow_root_node(view), node);
 
         // store the shadow node in the view so we can remove it later
         auto view_data = std::make_unique<view_shadow_data>(node);
-        view->store_data( 
-            std::move(view_data), 
-            surface_data_name
-        );
+        view->store_data(std::move(view_data), surface_data_name);
 
         view->damage();
     }
 
-    void deinit_view( wayfire_view view )
-    {
+    void deinit_view(wayfire_view view) {
         auto view_data = view->get_data<view_shadow_data>(surface_data_name);
         if (view_data != nullptr) {
             wf::scene::remove_child(view_data->shadow_ptr);
@@ -145,16 +137,14 @@ class wayfire_shadows : public wf::per_output_plugin_instance_t
         }
     }
 
-    void fini() override
-    {
+    void fini() override {
         output->disconnect(&on_view_mapped);
         output->disconnect(&on_view_updated);
         output->disconnect(&on_view_tiled);
         output->disconnect(&on_focus_changed);
         output->disconnect(&on_view_unmapped);
 
-        for (auto& view : output->workspace->get_views_in_layer(wf::ALL_LAYERS))
-        {
+        for (auto &view : output->workspace->get_views_in_layer(wf::ALL_LAYERS)) {
             deinit_view(view);
         }
     }
